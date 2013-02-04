662e4303facbde4fcf8ada68fbbd51345edb0e37
Igor Khavkin <ikhavkin@gmail.com>
2013-02-05 00:19:34 +0200

    experimenting with IO :)

--- aget/src/net/codevolve/aget/AndroidGetActivity.java
+++ aget/src/net/codevolve/aget/AndroidGetActivity.java
@@ -5,9 +5,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.*;
 
 public class AndroidGetActivity extends Activity {
     private static final int SELECTED_FILE_TO_LOAD = 1000;
+    private Button loadListButton;
+    private Button goButton;
+    private ListView fileListView;
+    private ArrayAdapter<String> fileArrayAdapter;
 
     /**
      * Called when the activity is first created.
@@ -16,6 +25,13 @@ public class AndroidGetActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        loadListButton = (Button) findViewById(R.id.loadListButton);
+        goButton = (Button) findViewById(R.id.goButton);
+        fileListView = (ListView) findViewById(R.id.fileListView);
+
+        fileArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[0]);
+        fileListView.setAdapter(fileArrayAdapter);
     }
 
     /**
@@ -34,9 +50,56 @@ public class AndroidGetActivity extends Activity {
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECTED_FILE_TO_LOAD) {
                 String result = data.getDataString();
+
+                try {
+                    loadFilesList(result);
+                }
+                catch (IOException exc) {
+                    // todo: proper error handling
+                    throw new RuntimeException(exc);
+                }
             }
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void loadFilesList(String path) throws IOException {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(path);
+            Reader reader = null;
+
+            try {
+                reader = new InputStreamReader(inputStream);
+                BufferedReader bufReader = null;
+
+                try {
+                    bufReader = new BufferedReader(reader);
+
+                    // Read input file line by line into list view.
+                    while (true) {
+                        String line = bufReader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+
+                        fileArrayAdapter.add(line);
+                    }
+                } finally {
+                    if (bufReader != null) {
+                        bufReader.close();
+                    }
+                }
+            } finally {
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
     }
 }
