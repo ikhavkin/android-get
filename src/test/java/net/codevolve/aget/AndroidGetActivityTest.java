2175da98f89404fee37522f8cdf4ca7a59f9268a
Igor Khavkin <ikhavkin@gmail.com>
2013-02-09 21:33:01 +0200

    resolving buttons in integration test

--- src/test/java/net/codevolve/aget/AndroidGetActivityTest.java
+++ src/test/java/net/codevolve/aget/AndroidGetActivityTest.java
@@ -1,6 +1,9 @@
 package net.codevolve.aget;
 
+import android.os.Bundle;
+import android.widget.Button;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -8,11 +11,31 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 @RunWith(RobolectricTestRunner.class)
+/**
+ * Integration tests for @see AndroidGetActivity .
+ */
 public class AndroidGetActivityTest {
+    AndroidGetActivity activity;
+    private Button goButton;
+    private Button loadButton;
+
+    @Before
+    public void setUp() {
+        activity = new AndroidGetActivity();
+        activity.onCreate(Bundle.EMPTY);
+
+        goButton = (Button) activity.findViewById(R.id.goButton);
+        loadButton = (Button) activity.findViewById(R.id.loadListButton);
+    }
 
     @Test
     public void shouldHaveHappySmiles() throws Exception {
-        String appName = new AndroidGetActivity().getResources().getString(R.string.app_name);
+        String appName = activity.getResources().getString(R.string.app_name);
         assertThat(appName, equalTo("aget"));
+    }
+
+    @Test
+    public void When_clicking_Go_without_adding_files_it_should_not_throw() {
+        goButton.performClick();
     }
 }
